802bc07b63b92b1d8f939c3b017a680e38576915
Raagmanas Madhukar <raagmanasmadhukar1712@gmail.com>
2026-05-22 12:55:26 +0530

    Change App component to return null
    Removed the main return content of the App component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,3 @@
 export default function App() {
-  return (
-    <div
-      style={{
-        background: '#050816',
-        color: 'white',
-        minHeight: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: '2rem',
-      }}
-    >
-      WeldSight AI Frontend Online
-    </div>
-  )
+  return null;
 }
